f9c5f81c891d5fd3143d7b95c5bfe86bc00ed568
BharathkumarDhanya <41415634+BharathkumarDhanya@users.noreply.github.com>
2018-07-19 12:40:25 +0530

    Added two more files

---
+++ HelloWorldClass.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldClass {
+	
+	public void display()
+	{
+		System.out.println("Hello Bharath");
+	}
+
+}
